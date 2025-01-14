c1aea8384de622c4ed09bf45b91ad67f93cc4d8c
NikhilPal002 <nikhilpal2017@gmail.com>
2025-01-14 12:16:33 +0530

    add and update page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 
 import { HomeComponent } from './Components/home/home.component';
+import { GuestsComponent } from './features/GuestOper/guests/guests.component';
+import { UpdateGuestComponent } from './features/GuestOper/update-guest/update-guest.component';
 
 
 export const routes: Routes = [
@@ -9,5 +11,13 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'receptionist/add-guest',
+        component: GuestsComponent
+    },
+    {
+        path: 'receptionist/update-guest',
+        component: UpdateGuestComponent
     }
 ];
---
+++ src/app/features/GuestOper/update-guest/update-guest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-guest',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-guest.component.html',
+  styleUrl: './update-guest.component.css'
+})
+export class UpdateGuestComponent {
+
+}
